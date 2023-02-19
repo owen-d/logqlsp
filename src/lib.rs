df2c8a87d31563026f8f835bc120dcf3345fb71d
Owen Diehl <ow.diehl@gmail.com>
2023-02-18 20:29:46 -0800

    libmain&eval Signed-off-by: Owen Diehl <ow.diehl@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,27 @@
+use chumsky::{prelude::Simple, Parser};
+use logql::parser;
+
 pub mod logql;
+
 #[cfg(test)]
 mod logql_test;
+
+pub fn libmain() {
+    let src = std::fs::read_to_string(std::env::args().nth(1).unwrap()).unwrap();
+
+    match parser().parse(src) {
+        Ok(ast) => match eval(ast) {
+            Ok(output) => println!("{:#?}", output),
+            Err(eval_errs) => eval_errs
+                .into_iter()
+                .for_each(|e| println!("Evaluation error: {}", e)),
+        },
+        Err(parse_errs) => parse_errs
+            .into_iter()
+            .for_each(|e| println!("Parse error: {}", e)),
+    }
+}
+
+fn eval(ast: logql::Expr) -> Result<logql::Expr, Vec<Simple<char>>> {
+    Ok(ast)
+}
