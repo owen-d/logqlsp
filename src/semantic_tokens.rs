d27833aabd5f9115e2c6b80a9606fc5419aa36a7
Owen Diehl <ow.diehl@gmail.com>
2023-02-20 12:56:21 -0800

    semantic tokens impl Signed-off-by: Owen Diehl <ow.diehl@gmail.com>

--- src/semantic_tokens.rs
+++ src/semantic_tokens.rs
@@ -9,17 +9,43 @@ pub const CONTROL: SemanticTokenType = SemanticTokenType::new("control");
 
 pub const LEGEND_TYPE: &[SemanticTokenType] = &[STRING, IDENT, OPERATOR, CONTROL];
 
-pub fn semantic_token_from_ast(
-    ast: &Spanned<Expr>,
-    semantic_tokens: &mut Vec<InCompleteSemanticToken>,
-) -> Vec<InCompleteSemanticToken> {
+pub fn semantic_token_from_ast(ast: &Spanned<Expr>) -> Vec<InCompleteSemanticToken> {
+    let mut semantic_tokens = vec![];
     match &ast.0 {
         Expr::Error => {}
-        Expr::LogExpr(expr) => match expr {
-            LogExpr::Selector(s) => {
-              s.
-            },
+        Expr::LogExpr((expr, _)) => match expr {
+            LogExpr::Selector((sel, span)) => {
+                // Push two control characters for the curly braces
+                semantic_tokens.push(InCompleteSemanticToken {
+                    start: span.start,
+                    length: 1,
+                    token_type: LEGEND_TYPE.iter().position(|t| t == &CONTROL).unwrap(),
+                });
+                semantic_tokens.push(InCompleteSemanticToken {
+                    start: span.end - 1,
+                    length: 1,
+                    token_type: LEGEND_TYPE.iter().position(|t| t == &CONTROL).unwrap(),
+                });
+
+                sel.labels.iter().for_each(|(matcher, _)| {
+                    semantic_tokens.push(InCompleteSemanticToken {
+                        start: matcher.name.1.start,
+                        length: matcher.name.1.end - matcher.name.1.start,
+                        token_type: LEGEND_TYPE.iter().position(|t| t == &IDENT).unwrap(),
+                    });
+                    semantic_tokens.push(InCompleteSemanticToken {
+                        start: matcher.matcher_type.1.start,
+                        length: matcher.matcher_type.1.end - matcher.matcher_type.1.start,
+                        token_type: LEGEND_TYPE.iter().position(|t| t == &OPERATOR).unwrap(),
+                    });
+                    semantic_tokens.push(InCompleteSemanticToken {
+                        start: matcher.value.1.start,
+                        length: matcher.value.1.end - matcher.value.1.start,
+                        token_type: LEGEND_TYPE.iter().position(|t| t == &STRING).unwrap(),
+                    });
+                });
+            }
         },
     };
-    todo!();
+    semantic_tokens
 }
