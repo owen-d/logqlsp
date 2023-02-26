b8f1ee3eb324b383864b879f03edb7e0dc674eb5
Owen Diehl <ow.diehl@gmail.com>
2023-02-25 22:53:13 -0800

    lexer Signed-off-by: Owen Diehl <ow.diehl@gmail.com>

--- src/parser/lexer.rs
+++ src/parser/lexer.rs
@@ -1,14 +1,15 @@
 use std::marker::PhantomData;
 
 use nom::{
-    bytes::complete::{tag, take_until},
+    branch::alt,
+    bytes::complete::{tag, take_until, take_while},
     character::{
         complete::{char, multispace0, none_of, one_of},
-        is_newline,
+        is_alphabetic, is_alphanumeric, is_newline,
     },
     error::ParseError,
     sequence::{delimited, Tuple},
-    IResult,
+    IResult, Parser,
 };
 
 use super::utils::Span;
@@ -41,6 +42,7 @@ pub const SINGLE_CHAR_DELIMITERS: &'static [char] = &[
 pub const MULTI_CHAR_DELIMS: &'static [&'static str] = &["!=", "=~", "!~", "|="];
 
 pub struct Delimited<T> {
+    s: String,
     // a type to implement a trait which maps to the valid variants
     pub(crate) valid: PhantomData<T>,
 }
@@ -71,24 +73,81 @@ fn lex(input: Span) -> Result {
     // whitespace -> continue
     // other delim -> delim
     // else it's not a protected char, repeatedly accumulate to string
-    let multi_char_delims = tag::<_, _, LexErr>("foo");
-    let single_char_delimts = one_of::<Span, _, LexErr>(SINGLE_CHAR_DELIMITERS);
 
-    todo!()
+    let multi_char_delims = alt::<Span, _, LexErr, _>((tag("!="), tag("=~"), tag("!~"), tag("|=")))
+        .map(|s| {
+            vec![Token::Delimiter(Delimited {
+                s: s.to_string(),
+                valid: PhantomData,
+            })]
+        });
+
+    let single_char_delims = one_of::<Span, _, LexErr>(SINGLE_CHAR_DELIMITERS).map(|c| {
+        vec![Token::Delimiter(Delimited {
+            s: c.to_string(),
+            valid: PhantomData,
+        })]
+    });
+
+    // listed in terms of parsing precedence
+    let token = whitespaced(alt((
+        comment,
+        string,
+        multi_char_delims,
+        single_char_delims,
+        word,
+    )));
+
+    token(input)
+}
+
+fn string(input: Span) -> Result {
+    let (s, (_, string, _)) = (char('"'), take_until("\""), char('"')).parse(input)?;
+    Ok((
+        s,
+        vec![
+            Token::Delimiter(Delimited {
+                s: "\"".to_string(),
+                valid: PhantomData,
+            }),
+            Token::Word(string.to_string()),
+            Token::Delimiter(Delimited {
+                s: "\"".to_string(),
+                valid: PhantomData,
+            }),
+        ],
+    ))
+}
+
+fn word(input: Span) -> Result {
+    let is_valid = |c: char| c.is_alphanumeric() || c == '_';
+    take_while(is_valid)
+        .map(|c: Span| vec![Token::Word(c.to_string())])
+        .parse(input)
 }
 
 fn comment(input: Span) -> Result {
     let (s, (_, comment, _)) = (tag("#"), take_until("\n"), tag("\n")).parse(input)?;
-    todo!()
+    Ok((
+        s,
+        vec![
+            Token::Delimiter(Delimited {
+                s: "#".to_string(),
+                valid: PhantomData,
+            }),
+            Token::Word(comment.to_string()),
+        ],
+    ))
 }
 
 /// A combinator that takes a parser `inner` and produces a parser that also consumes both leading and
 /// trailing whitespace, returning the output of `inner`.
-fn whitespace<'a, F, O, E: ParseError<&'a str>>(
-    inner: F,
-) -> impl FnMut(&'a str) -> IResult<&'a str, O, E>
+fn whitespaced<I, F, O, E>(inner: F) -> impl FnMut(I) -> IResult<I, O, E>
 where
-    F: FnMut(&'a str) -> IResult<&'a str, O, E>,
+    F: FnMut(I) -> IResult<I, O, E>,
+    I: nom::InputLength + nom::InputTakeAtPosition,
+    <I as nom::InputTakeAtPosition>::Item: nom::AsChar + Clone,
+    E: ParseError<I>,
 {
     delimited(multispace0, inner, multispace0)
 }
